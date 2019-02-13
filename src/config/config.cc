32ff681eef228fa1902512d07e3300e54a92e0e7
Zachary Fakir <zachary.fakir@epita.fr>
2019-02-13 12:14:08 -0800

    [CONFIG] moving json handling into config

--- src/config/config.cc
+++ src/config/config.cc
@@ -2,19 +2,111 @@
 
 #include "error/not-implemented.hh"
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wswitch-enum"
-#pragma GCC diagnostic ignored "-Wswitch-default"
-#pragma GCC diagnostic ignored "-Winline"
-#ifdef __clang__
-#pragma GCC diagnostic ignored "-Wlogical-op-parentheses"
-#endif
-#include <json.hpp>
-#pragma GCC diagnostic pop
-
-using json = nlohmann::json;
-
 namespace http
 {
-    // FIXME
+// FIXME
+VHostConfig::VHostConfig(const std::string &ip, const int port,
+                         const std::string &server_name,
+                         const std::string &root,
+                         const std::string &default_file)
+    : ip_(ip), port_(port), server_name_(server_name),
+      root_(root), default_file_(default_file) {}
+
+void VHostConfig::print_VHostConfig(void)
+{
+    std::cout << "IP:\t\t" << ip_ << "\n"
+              << "PORT:\t\t" << port_ << "\n"
+              << "SERVER NAME:\t" << server_name_ << "\n"
+              << "ROOT: \t\t" << root_ << "\n"
+              << "DEFAULT FILE:\t" << default_file_ << '\n';
+}
+
+json get_json(const std::string &s)
+{
+    std::ifstream ifs(s);
+    if (!ifs.is_open())
+        return nullptr;
+    json j = json::parse(ifs);
+    return j;
+}
+
+nlohmann::json get_vhosts(nlohmann::json j)
+{
+    try
+    {
+        auto &arr = j.at("vhosts");
+        return arr;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Invalid json structure: no vhosts" << '\n';
+    }
+    return nullptr;
+}
+
+int check_vhost(nlohmann::json j)
+{
+    if (j.find("ip") == j.end() || !j.find("ip")->is_string())
+    {
+        std::cerr << "Invalid json structure: missing ip" << '\n';
+        return 0;
+    }
+    if (j.find("port") == j.end() || !j.find("port")->is_number())
+    {
+        std::cerr << "Invalid json structure: missing port" << '\n';
+        return 0;
+    }
+    if (j.find("server_name") == j.end() || !j.find("server_name")->is_string())
+    {
+        std::cerr << "Invalid json structure: missing server_name" << '\n';
+        return 0;
+    }
+    if (j.find("root") == j.end() || !j.find("root")->is_string())
+    {
+        std::cerr << "Invalid json structure: missing root" << '\n';
+        return 0;
+    }
+    return 1;
+}
+
+
+struct ServerConfig parse_configuration(const std::string &path)
+{
+    ServerConfig c = ServerConfig();
+    nlohmann::json j = get_json(path.c_str());
+    nlohmann::json j2 = get_vhosts(j);
+    for (auto vhost = j.begin(); vhost != j.end(); vhost++)
+    {
+        nlohmann::json cur = *vhost;
+        if (check_vhost(cur))
+        {
+            std::string ip_s = cur.at("ip").get<std::string>();
+            int port_i = cur.at("port").get<int>();
+            std::string serv_s = cur.at("server_name").get<std::string>();
+            std::string root_s = cur.at("root").get<std::string>();
+            try
+            {
+                std::string def_s = cur.at("default_file").get<std::string>();
+                VHostConfig v = VHostConfig(ip_s, port_i, serv_s, root_s, def_s);
+                c.vhosts_.emplace_back(v);
+            }
+            catch (const std::exception &e1)
+            {
+                try
+                {
+                    VHostConfig v = VHostConfig(ip_s, port_i, serv_s, root_s);
+                    c.vhosts_.emplace_back(v);
+                }
+                catch (const std::exception &e2)
+                {
+                    std::cerr << "Invalid json" << '\n';
+                }
+            }
+        }
+        else
+        {
+            throw std::exception();
+        }
+    }
+}
 } // namespace http
--- src/config/config.hh
+++ src/config/config.hh
@@ -6,10 +6,24 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch-enum"
+#pragma GCC diagnostic ignored "-Wswitch-default"
+#pragma GCC diagnostic ignored "-Winline"
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wlogical-op-parentheses"
+#endif
+#include <json.hpp>
+#pragma GCC diagnostic pop
+
+using json = nlohmann::json;
 
 namespace http
 {
-    /**
+/**
      * \struct VHostConfig
      * \brief Value object storing a virtual host configuration.
      *
@@ -17,46 +31,53 @@ namespace http
      * dedicated structure is required to store the information related to
      * each one of them.
      */
-    struct VHostConfig
-    {
-        VHostConfig() = default;
-        VHostConfig(const VHostConfig&) = default;
-        VHostConfig& operator=(const VHostConfig&) = default;
-        VHostConfig(VHostConfig&&) = default;
-        VHostConfig& operator=(VHostConfig&&) = default;
+struct VHostConfig
+{
+    VHostConfig(const std::string &ip, const int port,
+                const std::string &server_name, const std::string &root,
+                const std::string &default_file);
+    //VHostConfig() = default;
+    VHostConfig(const VHostConfig &) = default;
+    VHostConfig &operator=(const VHostConfig &) = default;
+    VHostConfig(VHostConfig &&) = default;
+    VHostConfig &operator=(VHostConfig &&) = default;
 
-        ~VHostConfig() = default;
+    ~VHostConfig() = default;
+    void print_VHostConfig(void);
 
-        // FIXME: Add members to store the information relative to a vhost.
-    };
+    const std::string ip_;
+    const int port_;
+    const std::string server_name_;
+    const std::string root_;
+    const std::string default_file_;
+};
 
-    /**
+/**
      * \struct ServerConfig
      * \brief Value object storing the server configuration.
      *
      * To avoid opening the configuration file each time we need to access the
      * server configuration, a dedicated structure is required to store it.
      */
-    struct ServerConfig
-    {
-        ServerConfig() = default;
-        ServerConfig(const ServerConfig&) = default;
-        ServerConfig& operator=(const ServerConfig&) = default;
-        ServerConfig(ServerConfig&&) = default;
-        ServerConfig& operator=(ServerConfig&&) = default;
+struct ServerConfig
+{
+    ServerConfig() = default;
+    ServerConfig(const ServerConfig &) = default;
+    ServerConfig &operator=(const ServerConfig &) = default;
+    ServerConfig(ServerConfig &&) = default;
+    ServerConfig &operator=(ServerConfig &&) = default;
 
-        ~ServerConfig() = default;
+    ~ServerConfig() = default;
 
-        /* FIXME: Add members to store the information relative to the
-           configuration of the server. */
-    };
+    std::vector<http::VHostConfig> vhosts_;
+};
 
-    /**
+/**
      * \brief Parse the server configuration file.
      *
      * \param path string containing the path to the server configuration
      * file.
      * \return The server configuration.
      */
-    struct ServerConfig parse_configuration(const std::string& path);
+struct ServerConfig parse_configuration(const std::string &path);
 } // namespace http
