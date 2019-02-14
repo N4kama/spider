f9c8038bec35d442e7b853ef973fcd0b2807b4df
Amine Heffar <amine.heffar@epita.fr>
2019-02-13 16:11:04 -0800

    [RESPONSE] head methode implem but not tested

--- src/request/response.cc
+++ src/request/response.cc
@@ -70,11 +70,41 @@ void http_head(struct Request r, DefaultSocket socketClient)
     heads << r.headers.end()->second;
 
     str << "HEAD / HTTP/1.1\r\nStatus: " << r.status << " Headers: " << heads << " \r\nConnection: close\r\n\r\n";
-    if (socketServer.send(&str, str..str().size()) < 0)
+    if (socketServer.send(&str, str.str().size()) < 0)
         socketClient.send("<html><h1>http error: 520</h1><h2> Ton url pue la merde</h2></html>", 67);
     std::cout << "HEAD done\n";
 }
 
+void http_post(struct Request r, DefaultSocket socketClient)
+{
+    std::cout << "POST " << r.url.c_str() << '\n';
 
+    struct sockaddr_in sock;
+    struct hostent *host;
+
+    host = gethostbyname(r.url.c_str());
+    if (!host)
+    {
+        socketClient.send("<html><h1>http error: 404</h1><h2> Ton url pue la merde</h2></html>", 67);
+        return;
+    }
+
+    int port = 80;
+    sock.sin_port = htons(port);
+    sock.sin_family = AF_INET;
+    sock.sin_addr.s_addr = *(reinterpret_cast<unsigned long *>(host->h_addr));//*((unsigned long*)host->h_addr);
+
+    DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
+    std::cout << "connexion done!\n";
+
+    std::stringstream str;
+
+    while (socketClient.recv(&str, 500) >= 0)
+    {
+        std::cout << str.str();
+    }
+    std::cout << "\nHEAD done\n";
+}
 
 } // namespace http
