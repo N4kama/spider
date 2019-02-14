1901b7a7bba67d58e6920b2500dde036e8844f63
Amine Heffar <amine.heffar@epita.fr>
2019-02-14 02:28:02 -0800

    [RESPONSE] methode get head post use only one socket, no more socket serv creation in it

--- src/request/response.cc
+++ src/request/response.cc
@@ -8,7 +8,7 @@
 
 namespace http
 {
-void http_get(struct Request r, DefaultSocket socketClient)
+void http_get(struct Request r, DefaultSocket socketClient, int port)
 {
     std::cout << "GET " << r.url.c_str() << '\n';
 
@@ -22,23 +22,25 @@ void http_get(struct Request r, DefaultSocket socketClient)
         return;
     }
 
-    int port = 80;
     sock.sin_port = htons(port);
     sock.sin_family = AF_INET;
     sock.sin_addr.s_addr = *(reinterpret_cast<unsigned long *>(host->h_addr));//*((unsigned long*)host->h_addr);
 
-    DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
-    std::cout << "connexion done!\n";
+    //DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
+    //std::cout << "connexion done!\n";
 
     std::stringstream str;
     str << "GET / HTTP/1.1\r\nHost: " << r.url.c_str() << " \r\nConnection: close\r\n\r\n";
-    if (socketServer.send(&str, str.str().size()) < 0)
-        socketClient.send("<html><h1>http error: 520</h1><h2> Ton url pue la merde</h2></html>", 67);
+    if (socketClient.send(&str, str.str().size()) < 0)
+    {
+        std::cout << "Client disconnected !\n";
+        //socketClient.send("<html><h1>http error: 520</h1><h2> Le serv arrive pas a me rep</h2></html>", 67);
+    }
     std::cout << "GET done\n";
 }
 
-void http_head(struct Request r, DefaultSocket socketClient)
+void http_head(struct Request r, DefaultSocket socketClient, int port)
 {
     std::cout << "HEAD " << r.url.c_str() << '\n';
 
@@ -52,14 +54,13 @@ void http_head(struct Request r, DefaultSocket socketClient)
         return;
     }
 
-    int port = 80;
     sock.sin_port = htons(port);
     sock.sin_family = AF_INET;
     sock.sin_addr.s_addr = *(reinterpret_cast<unsigned long *>(host->h_addr));//*((unsigned long*)host->h_addr);
 
-    DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
-    std::cout << "connexion done!\n";
+    //DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
+    //std::cout << "connexion done!\n";
 
     std::stringstream str;
     std::stringstream heads;
@@ -70,12 +71,13 @@ void http_head(struct Request r, DefaultSocket socketClient)
     heads << r.headers.end()->second;
 
     str << "HEAD / HTTP/1.1\r\nStatus: " << r.status << " Headers: " << heads << " \r\nConnection: close\r\n\r\n";
-    if (socketServer.send(&str, str.str().size()) < 0)
-        socketClient.send("<html><h1>http error: 520</h1><h2> Ton url pue la merde</h2></html>", 67);
+    if (socketClient.send(&str, str.str().size()) < 0)
+        std::cout << "Client disconnected !\n";
+        //socketClient.send("<html><h1>http error: 520</h1><h2> Le serv arrive pas à m'envoyer</h2></html>", 67);
     std::cout << "HEAD done\n";
 }
 
-void http_post(struct Request r, DefaultSocket socketClient)
+void http_post(struct Request r, DefaultSocket socketClient, int port)
 {
     std::cout << "POST " << r.url.c_str() << '\n';
 
@@ -89,14 +91,13 @@ void http_post(struct Request r, DefaultSocket socketClient)
         return;
     }
 
-    int port = 80;
     sock.sin_port = htons(port);
     sock.sin_family = AF_INET;
     sock.sin_addr.s_addr = *(reinterpret_cast<unsigned long *>(host->h_addr));//*((unsigned long*)host->h_addr);
 
-    DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
-    std::cout << "connexion done!\n";
+    //DefaultSocket socketServer = DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //socketServer.connect((sockaddr *) &sock, sizeof(sockaddr));
+    //std::cout << "connexion done!\n";
 
     std::stringstream str;
 
