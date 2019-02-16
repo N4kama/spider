9d05021b968225703c35b2050930e9261ab1fd41
Zachary Fakir <zachary.fakir@epita.fr>
2019-02-16 18:07:12 +0100

    [MAIM] updated main

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,5 @@
 #include "main.hh"
-
+/*
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
@@ -7,7 +7,7 @@ int parsing_json(std::string arg, int debug)
 {
     try
     {
-        ServerConfig s = parse_configuration(arg, debug);
+        http::ServerConfig s = http::parse_configuration(arg, debug);
     }
     catch (const std::exception &e)
     {
@@ -50,4 +50,38 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
+}
+*/
+
+#include <request/request.hh>
+#include <request/response.hh>
+#include <misc/fd.hh>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main()
+{
+    char *localip;
+    hostent *localHost;
+    struct sockaddr_in adr;
+    auto sock = http::DefaultSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    localHost = gethostbyname("hello.html");
+    localip = inet_ntoa(*(struct in_addr *)*localHost->h_addr_list);
+
+    adr.sin_family = AF_INET;
+    adr.sin_addr.s_addr = inet_addr(localip);
+    adr.sin_port = htons(1234);
+
+    sock.bind((sockaddr *)&adr, sizeof(adr));
+
+    auto r = http::Request();
+    r.method = "get";
+    r.status = 100;
+    r.url = "hello.html";
+
+    http_get(r, sock);
+    return 1;
 }
