45efaaa989d3cb100a7638162589a0bc5c0f90b7
Amine Heffar <amine.heffar@epita.fr>
2019-02-19 13:52:31 -0800

    [SOCKET] fixed accept func + using name sapace sys

--- src/socket/default-socket.cc
+++ src/socket/default-socket.cc
@@ -26,58 +26,60 @@ void DefaultSocket::listen(int backlog)
 
 ssize_t DefaultSocket::recv(void *dst, size_t len)
 {
-    return ::recv(*fd_, dst, len, 0);
+    return sys::recv(*fd_, dst, len, 0);
 }
 
 ssize_t DefaultSocket::send(const void *src, size_t len)
 {
-    return ::send(*fd_, src, len, 0);
+    return sys::send(*fd_, src, len, 0);
 }
 
 ssize_t
 DefaultSocket::sendfile(misc::shared_fd &in_fd, off_t &offset, size_t c)
 {
     int in = *in_fd;
-    return ::sendfile(*fd_, in, &offset, c);
+    return sys::sendfile(*fd_, in, &offset, c);
 }
 
 void DefaultSocket::bind(const sockaddr *addr, socklen_t addrlen)
 {
     if (!addr)
+    {
+        std::cerr << "bind: bad address given !\n";
         return;
+    }
 
     int fd = *fd_;
-    if (::bind(fd, addr, addrlen) < 0)
-        std::cerr << std::strerror(errno) << '\n';
+    sys::bind(fd, addr, addrlen);
 }
 
 void DefaultSocket::setsockopt(int level, int optname, int optval)
 {
     int fd = *fd_;
     char *opt_buf = new char[optval];
-    if (::setsockopt(fd, level, optname, opt_buf, optval) < 0)
-        std::cerr << std::strerror(errno) << '\n';
+    sys::setsockopt(fd, level, optname, opt_buf, optval);
 }
 
 std::shared_ptr<Socket> DefaultSocket::accept(sockaddr *addr,
                                               socklen_t *addrlen)
 {
-    if (!addr)
+     if (!addr)
+    {
+        std::cerr << "accept: bad address given !\n";
         return nullptr;
-
+    }
+    
     int fd = *fd_;
-    if (::accept(fd, addr, addrlen) < 0)
-        return nullptr;
-
-    auto res = std::make_shared<DefaultSocket>(DefaultSocket(fd_));
+    int r_res = sys::accept(fd, addr, addrlen);
+    misc::shared_fd f = std::make_shared<misc::FileDescriptor>(r_res);
+    auto res = std::make_shared<DefaultSocket>(DefaultSocket(f));
     return res;
 }
 
 void DefaultSocket::connect(const sockaddr *addr, socklen_t l)
 {
     int fd = *fd_;
-    if (::connect(fd, addr, l) < 0)
-        std::cerr << std::strerror(errno) << '\n';
+    sys::connect(fd, addr, l);
 }
 
 } // namespace http
