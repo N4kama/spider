b9df9ac66897647ceb185e037241c696eafab5c2
Zachary Fakir <zachary.fakir@epita.fr>
2019-02-20 21:39:48 +0100

    [MAIN] Fix all main bug except dispatcher

--- src/main.cc
+++ src/main.cc
@@ -3,46 +3,70 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/epoll.h>
 #include <request/response.hh>
+#include <sys/epoll.h>
+#include <misc/fd.hh>
+#include "events/register.hh"
+#include "events/listener.hh"
+#include "socket/socket.hh"
+
+
 namespace po = boost::program_options;
+using namespace http;
+EventWatcherRegistry event_register;
 
 int dispatch(std::string arg, int debug)
 {
     try
     {
         //Init one VHOST
-        http::ServerConfig config = http::parse_configuration(arg, debug);
-        http::VHostConfig vhost = config.vhosts_.at(0);
+        ServerConfig config = parse_configuration(arg, debug);
+        VHostConfig vhost_config = config.vhosts_.at(0);
         //Add the new vhost to the VHOST list in the dispatcher
-        http::dispatcher.add_vhost(vhost);
+        dispatcher.add_vhost(vhost_config);
 
         //init socket
-        http::DefaultSocket server_socket = http::DefaultSocket(AF_INET, SOCK_STREAM, 0);
+        DefaultSocket server_socket = DefaultSocket(AF_INET, SOCK_STREAM, 0);
         struct sockaddr_in addr;
-        int len = sizeof(addr);
 
         //init address
         addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = INADDR_ANY;
-        addr.sin_port = htons(vhost.port_);
+        addr.sin_addr.s_addr = inet_addr(vhost_config.ip_.c_str());
+        addr.sin_port = htons(vhost_config.port_);
 
         //bind socket with address
         struct sockaddr *addr_not_in = (struct sockaddr *)&addr;
-        server_socket.bind(addr_not_in, len);
+        server_socket.bind(addr_not_in, sizeof(addr));
         server_socket.listen(30);
-        socklen_t *s_len = (socklen_t *)&len;
-        char buffer[5];
-        while (1)
-        {
-            http::shared_socket new_s = server_socket.accept(addr_not_in, s_len);
-            http::DefaultSocket sock = http::DefaultSocket(new_s->fd_get());
-            std::cout << "WOW ANOTHER VICTORY FOR RAYOU!!!\n";
-            while(1)
-            {
-                sock.recv(buffer, 5);
-                std::cout << buffer;
-            }
-        }
+        //socklen_t *s_len = (socklen_t *)&len;
+
+        //event handling
+        //EventLoop e_loop = EventLoop();
+        //std::shared_ptr<struct FileDescriptor> s_fd = std::make_shared<FileDescriptor>(server_socket.fd_get());
+        //std::shared_ptr<DefaultSocket> s_server = std::make_shared<DefaultSocket>(s_fd);
+        //ListenerEW e_server = ListenerEW(s_server);
+        //e_loop.register_watcher(&e_server);
+
+        //event loop
+        //int res;
+        //while (1)
+        //{
+        //    res = epoll_wait();
+        //    for (int n = 0; n < res; n++)
+        //    {
+                /*
+                if ()
+                {
+                    http::shared_socket new_s = server_socket.accept(addr_not_in, s_len);
+                    http::DefaultSocket sock = http::DefaultSocket(new_s->fd_get());
+                }
+                else
+                {
+                }
+                */
+        //    }
+        //}
         //DEBUG TESTING:
     }
     catch (const std::exception &e)
