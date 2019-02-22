be099daa6f8fe0780a870bfc154fc6e3aeb5c4be
Amine Heffar <amine.heffar@epita.fr>
2019-02-22 23:08:19 +0100

    [EVENTS][LISTENER] correction eventwatcher explicit constructor + listner constructor

--- src/events/listener.cc
+++ src/events/listener.cc
@@ -2,8 +2,8 @@
 namespace http
 {
     ListenerEW::ListenerEW(shared_socket socket)
+        : sock_{socket}
     {
-        sock_ = socket;
         struct sockaddr_in my_addr;
         socklen_t len = sizeof(my_addr);
         getsockname(socket->fd_get()->fd_, (struct sockaddr *) &my_addr, &len);
