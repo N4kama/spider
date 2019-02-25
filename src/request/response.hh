b1ec9cd77b672cdedcb4484484b8cb42acb4a217
Amine Heffar <amine.heffar@epita.fr>
2019-02-25 20:55:20 +0100

    [RESPONSE] new implem for method head and response constructor done

--- src/request/response.hh
+++ src/request/response.hh
@@ -25,11 +25,13 @@ namespace http
         Response(Response&&) = default;
         Response& operator=(Response&&) = default;
         ~Response() = default;
-        void http_rget(struct Request r);
+
         void http_rhead(struct Request r);
+        void http_rget(struct Request r);
         void http_rpost(struct Request r);
+
         STATUS_CODE status_code;
-        int variable_juste_pour_test;
+        std::string rep;
     };
 
     void parse_uri(std::string uri, std::string& servN, std::string& fileP);
