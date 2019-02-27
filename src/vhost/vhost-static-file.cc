aef0fe8d3888c82241af84846e03d4621cc6f602
Romain Baguet <romain.baguet@epita.fr>
2019-02-27 18:25:24 +0100

    [EVENTS] vhost static file now handle events

--- src/vhost/vhost-static-file.cc
+++ src/vhost/vhost-static-file.cc
@@ -14,10 +14,9 @@ VHostStaticFile::VHostStaticFile(const VHostConfig& ext_conf)
 void VHostStaticFile::respond(const Request& req, Connection conn,
                               remaining_iterator, remaining_iterator)
 {
-    conn.rep_ = std::make_shared<Response>(Response(req));
     event_register.register_ew<http::SendEv, http::shared_socket,
-                        std::shared_ptr<Response>>(conn.sock_,
-                        conn.rep_);
-    conn.sock_->send(conn.rep_->rep.c_str(), conn.rep_->rep.length());
+                        std::shared_ptr<Response>>(
+                        std::make_shared<DefaultSocket>(conn.sock_),
+                        std::make_shared<Response>(req));
     // Call send_reponse veentwatcher with connexion struct
 }
