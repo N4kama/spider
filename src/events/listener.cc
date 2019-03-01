ba3f56e660d7114f29bedfdbf8f8ad4cadabbb51
Amine Heffar <amine.heffar@epita.fr>
2019-03-01 02:42:09 +0100

    [SOCKET] ipv6 is now set on server and client socket

--- src/events/listener.cc
+++ src/events/listener.cc
@@ -21,14 +21,15 @@ namespace http
     {
         struct sockaddr_in addr;
         socklen_t addr_len = sizeof(addr);
-        std::shared_ptr<Socket> new_s =
-            sock_->accept((struct sockaddr*)&addr, &addr_len);
+        std::shared_ptr<DefaultSocket> new_s =
+            std::make_shared<http::DefaultSocket>(
+                sock_->accept((struct sockaddr*)&addr, &addr_len)->fd_get());
+        new_s->ipv6_set(addr.sin_family == AF_INET6);
         std::cout << "Successfully connected with client.\n";
         // struct ev_io* w_client = new ev_io(); // = ?
         // ev_io_init(w_client, read_cb, new_s->fd_get()->fd_, EV_READ);
         // ev_io_start(event_register.loop_get().loop, w_client);
-        event_register.register_ew<http::RecvEv, http::shared_socket>(
-            std::make_shared<http::DefaultSocket>(new_s->fd_get()));
+        event_register.register_ew<http::RecvEv, http::shared_socket>(new_s);
         // new_connexion(sock_);
     }
 } // namespace http
