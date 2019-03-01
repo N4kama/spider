3f44c5d6855e196e704b6621d5a9aa8df4d929f1
Amine Heffar <amine.heffar@epita.fr>
2019-03-01 01:59:54 +0100

    [VHOST] dispatcher nearly done but some case on ipv6 are buggy

--- src/vhost/dispatcher.cc
+++ src/vhost/dispatcher.cc
@@ -15,31 +15,47 @@ namespace http
         // Now we consider there is only one vhost
         try
         {
+            uint16_t port = 0;
+            std::string host;
+            struct in_addr addr;
             if (cnx.sock_->is_ipv6())
             {
                 struct sockaddr_in6 my_addr;
                 socklen_t len = sizeof(my_addr);
                 getsockname(cnx.sock_->fd_get()->fd_,
-                            (struct sockaddr_in6*)&my_addr, &len);
+                            (struct sockaddr*)&my_addr, &len);
+                port = ntohs(my_addr.sin6_port);
                 char str[INET6_ADDRSTRLEN];
-                inet_ntop(AF_INET6, &my_addr.sin6_addr, str, INET6_ADDRSTRLEN);
+                host = inet_ntop(AF_INET6, &my_addr.sin6_addr, str,
+                                 INET6_ADDRSTRLEN);
             } else
             {
                 struct sockaddr_in my_addr;
                 socklen_t len = sizeof(my_addr);
                 getsockname(cnx.sock_->fd_get()->fd_,
                             (struct sockaddr*)&my_addr, &len);
-                uint16_t port = ntohs(my_addr.sin_port);
+                port = ntohs(my_addr.sin_port);
                 char str[INET_ADDRSTRLEN];
-                inet_ntop(AF_INET, &my_addr.sin_addr, str, INET_ADDRSTRLEN);
+                host =
+                    inet_ntop(AF_INET, &my_addr.sin_addr, str, INET_ADDRSTRLEN);
             }
-            // search to whom vhost the request is destined
-            auto vhost = (http::dispatcher.vhosts_[0]);
-            cnx.req_.config_ptr =
-                std::make_shared<VHostConfig>(vhost->get_conf());
-            cnx.req_.get_path(); // initialise path_config (now that config_ptr
-                                 // is created)
-            vhost->respond(cnx.req_, cnx, 0, 0);
+            for (unsigned i = 0; i < http::dispatcher.vhosts_.size(); i++)
+            {
+                auto vhost = (http::dispatcher.vhosts_[i]);
+                inet_aton(vhost->get_conf().ip_.c_str(), &addr);
+                std::string temp_host = std::string(inet_ntoa(addr));
+                if ((temp_host == host)
+                    && port == vhost->get_conf().port_)
+                {
+                    cnx.req_.config_ptr =
+                        std::make_shared<VHostConfig>(vhost->get_conf());
+                    cnx.req_.get_path(); // initialise path_config (now that
+                                         // config_ptr is created)
+                    vhost->respond(cnx.req_, cnx, 0, 0);
+                    break;
+                }
+            }
+
         } catch (const std::exception& e)
         {
             std::cerr << e.what() << '\n';
