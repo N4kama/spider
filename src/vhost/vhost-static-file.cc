086d6159b6c97a78fa384977494bb6ad548c2917
Rayane Amrouche <rayane.amrouche@epita.fr>
2019-03-23 20:27:08 +0100

    [EVENT] added forward to vhost static file too

--- src/vhost/vhost-static-file.cc
+++ src/vhost/vhost-static-file.cc
@@ -14,10 +14,8 @@ VHostStaticFile::VHostStaticFile(const VHostConfig& ext_conf)
 void VHostStaticFile::respond(const Request& req, Connection conn,
                               remaining_iterator, remaining_iterator)
 {
-    std::shared_ptr<DefaultSocket> new_s =
-        std::make_shared<DefaultSocket>(conn.sock_->fd_get());
-    new_s->set_vhost(conn.sock_->get_vhost());
     event_register.register_ew<http::SendEv, http::shared_socket,
                                std::shared_ptr<Response>>(
-        new_s, std::make_shared<Response>(req));
+        std::forward<shared_socket>(conn.sock_),
+        std::make_shared<Response>(req));
 }
