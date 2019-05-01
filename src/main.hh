6cca9fc57e5d89698483bed5c94143a63f29502f
Zachary Fakir <zachary.fakir@epita.fr>
2019-05-01 15:57:06 +0200

    [GENERAL] added functions to main.hh

--- src/main.hh
+++ src/main.hh
@@ -17,4 +17,11 @@ namespace http
 {
     extern http::Dispatcher dispatcher;
     int start_server(std::string arg);
+    void init_ssl();
+    void init_vhost(http::ServerConfig config);
+    void
+    init_listeners(std::vector<std::shared_ptr<http::ListenerEW>>& listeners,
+                   http::ServerConfig config, int i);
+    void end_loop(std::vector<std::shared_ptr<http::ListenerEW>>& listeners);
+
 } // namespace http
